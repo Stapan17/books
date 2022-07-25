d9416f4b8c07070f7497e921fb443f573c16a328
Stapan17 <savanitapan2001@gmail.com>
2022-07-25 10:18:58 +0530

    version 2

--- app/urls.py
+++ app/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('book-lists', views.books_lists, name='book-lists'),
+    path('', views.books_lists, name='book-lists'),
     path('add-book', views.add_book, name='add-book'),
     path('update-book/<int:pk>', views.update_book, name='update-book'),
     path('delete-book/<int:pk>', views.delete_book, name='delete-book'),
