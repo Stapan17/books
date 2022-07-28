8dae45b751243a5a1c25bc532b4d3e1a172e9b35
Stapan17 <savanitapan2001@gmail.com>
2022-07-28 16:29:06 +0530

    testing

--- deploy.sh
+++ deploy.sh
@@ -1,21 +1,19 @@
-# heroku
-cp -r ~/.ssh/ ~/
-HEROKU_API_KEY="2c794a48-a1f3-46f6-ba7a-d90a3f98a5f0" heroku git:remote -a docker-books-container
-git remote -v
-git remote show
+# # heroku
+# cp -r ~/.ssh/ ~/
+# HEROKU_API_KEY="2c794a48-a1f3-46f6-ba7a-d90a3f98a5f0" heroku git:remote -a docker-books-container
+# git remote -v
+# git remote show
 
-echo "ok1234"
-# git remote set-url origin 'https://Stapan17:git@github.com/Stapan17/books.git'
+# echo "ok1234"
+# # git remote set-url origin 'https://Stapan17:git@github.com/Stapan17/books.git'
 
-# git remote set-url origin git@github.com:Stapan17/books.git
-git remote set-url heroku git@heroku.com:Stapan17/docker-books-container.git
+# # git remote set-url origin git@github.com:Stapan17/books.git
+# git remote set-url heroku git@heroku.com:Stapan17/docker-books-container.git
 
-git remote show origin
-echo "ok2"
-# git remote show heroku
-echo "ok3"
-git pull heroku main
-echo "ok in"
-git push heroku origin/dev:main
-echo "ok4"
+# git remote show origin
+# echo "ok2"
+# echo "ok3"
 
+# echo "ok in"
+git push heroku dev:main
+# echo "ok4"
