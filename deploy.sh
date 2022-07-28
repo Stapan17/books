d25832bbd88812627a4a1c6484c701e819ceeb2a
Stapan17 <savanitapan2001@gmail.com>
2022-07-28 11:08:21 +0530

    testing

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 git remote -v 
 git remote show
-git remote show origin
+# git remote show origin
 git remote show origin1
-git push heroku refs/heads/origin/dev:refs/heads/origin1/main
+git push heroku origin/dev:origin1/main
