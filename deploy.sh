68d1a9e25c10781cd7cc9f0de0f39deae9addc72
Stapan17 <savanitapan2001@gmail.com>
2022-07-28 13:40:17 +0530

    testing

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,11 @@
 HEROKU_API_KEY="2c794a48-a1f3-46f6-ba7a-d90a3f98a5f0" heroku git:remote -a docker-books-container
 # git remote -v
 # git remote show
-# git remote show origin
+echo "ok1"
+git remote show origin
+echo "ok2"
 git remote show heroku
+echo "ok3"
 git push heroku origin/dev:heroku/main
+echo "ok4"
+
