ec2d6e2dfec0808c16b3fca2eeed78490259f108
Stapan17 <savanitapan2001@gmail.com>
2022-07-28 14:20:29 +0530

    testing

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 # heroku
 HEROKU_API_KEY="2c794a48-a1f3-46f6-ba7a-d90a3f98a5f0" heroku git:remote -a docker-books-container
-# git remote -v
-# git remote show
+git remote -v
+git remote show
 
 echo "ok1234"
 git remote set-url origin 'https://Stapan17:git@github.com/Stapan17/books.git'
