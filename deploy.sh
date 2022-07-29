27aec7ef33c81bf2588e71354d74d6b5a671e286
Stapan17 <savanitapan2001@gmail.com>
2022-07-29 12:58:25 +0530

    test

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,9 @@
-git remote -v
-git remote rm heroku
-git remote add heroku git@heroku.com:docker-books-container.git
+# git remote -v
+# git remote rm heroku
+# git remote add heroku git@heroku.com:docker-books-container.git
 
 git checkout dev
 git pull origin dev
 
+git status
 git push heroku dev:main
